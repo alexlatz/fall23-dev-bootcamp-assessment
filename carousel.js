ccad64cb9209c98b5cfdcfb3896b6e27dbb551d2
Alex Latz <alexlatz@gatech.edu>
2023-08-28 13:17:48 -0400

    finished demo arrows, text

--- carousel.js
+++ carousel.js
@@ -7,20 +7,28 @@ lead.forEach(l => l.addEventListener('scroll', e => {
     });
 }));
 
+
 /*
 This could be done without JS, just had to do this because 
 of a Chrome bug with HTML links to smooth scroll elements
 */
-var slide = 1
+
+var next = document.querySelector(".next-arrow");
+var prev = document.querySelector(".prev-arrow");
+var slide = 1;
 function nextSlide() {
-    if (slide == 5) return
-    slide++
-    var pic = document.getElementById("slide" + slide)
-    pic.parentElement.scrollBy(pic.children[0].width, 0)
+    if (slide == 5) return;
+    if (slide == 1) prev.classList.remove('arrow-inactive');
+    slide++;
+    var pic = document.getElementById("slide" + slide);
+    pic.parentElement.scrollBy(pic.children[0].width, 0);
+    if (slide == 5) next.classList.add('arrow-inactive');
 }
 function prevSlide() {
-    if (slide == 1) return
-    slide--
-    var pic = document.getElementById("slide" + slide)
-    pic.parentElement.scrollBy(-pic.children[0].width, 0)
+    if (slide == 1) return;
+    if (slide == 5) next.classList.remove('arrow-inactive');
+    slide--;
+    var pic = document.getElementById("slide" + slide);
+    pic.parentElement.scrollBy(-pic.children[0].width, 0);
+    if (slide == 1) prev.classList.add('arrow-inactive');
 }
